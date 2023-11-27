688b8635530a88b7e47fc5182fd4a28e1fc449f7
bkchandan18 <132631864+bkchandan18@users.noreply.github.com>
2023-11-27 23:48:28 +0000

    container config

---
+++ basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountid =123645;
+let acccountemail ="ckumar@gmail.com";
+var accountpasssward ="181818"
+accountcity ="indore";
+// accountid=1; not allowed
+console.log(accountid);
+console.table([accountid,acccountemail,accountpasssward,accountcity])
