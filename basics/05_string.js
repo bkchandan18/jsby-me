d3f1ed42cf2f9e6971f763e2b59fe20b49d5175a
bkchandan18 <132631864+bkchandan18@users.noreply.github.com>
2023-12-01 05:17:21 +0000

    string method

---
+++ basics/05_string.js
@@ -0,0 +1,19 @@
+// const name ="merababa"
+const name ="chandan"
+const num =30
+// console.log(name +num+"value"); not recommended
+// agar concatinate karna hai use bactics
+// not use double or single quote for concatinate this is calledsring interpolation 
+console.log(`hello my name is ${name} and my fav num is  ${num} `);
+// new way to declere string where new is object
+const myname = new String('chandan')
+// access key value paire
+console.log(myname[0]);
+// // print c
+// console.log(myname.length);
+// console.log(myname.toUpperCase());
+// character kis position pe hai charAt()method se..
+console.log(myname.charAt(2));
+// whitch position indexOf
+console.log(myname.indexOf('h'));
+
